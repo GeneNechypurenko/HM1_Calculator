189a37945f429b02d752369614e99deaeaf1708a
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-11-15 15:20:35 +0200

    Add project files.

---
+++ HM1_Calculator/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HM1_Calculator
+{
+    public partial class MainWindow : Window
+    {
+        private string currentNumber = string.Empty;
+        private string previousOperation = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string content = button.Content.ToString();
+
+            switch (content)
+            {
+                case "C":
+                    ClearAll();
+                    break;
+                case "CE":
+                    ClearEntry();
+                    break;
+                case "<-":
+                    Backspace();
+                    break;
+                case "=":
+                    CalculateResult();
+                    break;
+                default:
+                    AppendToCurrentNumber(content);
+                    break;
+            }
+        }
+
+        private void OperatorButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            char operation = button.Content.ToString()[0];
+
+            if (!string.IsNullOrEmpty(currentNumber))
+            {
+                if (!string.IsNullOrEmpty(previousOperation))
+                {
+                    CalculateResult();
+                    UpdatePreviousOperationTextBox();
+                }
+                else { previousOperation = currentNumber; }
+
+                currentNumber = string.Empty;
+                UpdateCurrentNumberTextBox();
+
+                if (Array.IndexOf(new[] { '+', '-', '*', '/' }, operation) != -1)
+                {
+                    previousOperation += $" {operation} ";
+                    UpdatePreviousOperationTextBox();
+                }
+            }
+        }
+        private void ClearAll()
+        {
+            currentNumber = string.Empty;
+            previousOperation = string.Empty;
+            UpdateCurrentNumberTextBox();
+            UpdatePreviousOperationTextBox();
+        }
+        private void ClearEntry()
+        {
+            currentNumber = string.Empty;
+            UpdateCurrentNumberTextBox();
+        }
+        private void Backspace()
+        {
+            if (currentNumber.Length > 0)
+            {
+                currentNumber = currentNumber.Substring(0, currentNumber.Length - 1);
+                UpdateCurrentNumberTextBox();
+            }
+        }
+        private void AppendToCurrentNumber(string value)
+        {
+            currentNumber += value;
+            UpdateCurrentNumberTextBox();
+        }
+        private void CalculateResult()
+        {
+            double result = Convert.ToDouble(new System.Data.DataTable().Compute($"{previousOperation} {currentNumber}", ""));
+            currentNumber = result.ToString();
+            UpdateCurrentNumberTextBox();
+            previousOperation = string.Empty;
+            UpdatePreviousOperationTextBox();
+        }
+        private void UpdateCurrentNumberTextBox() => currentNumberTextBox.Text = currentNumber;
+        private void UpdatePreviousOperationTextBox() => previousOperationTextBox.Text = previousOperation;
+    }
+}
---
+++ Preferences/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Preferences
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowPreferencesButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sportRadioButton.IsChecked == true) { resultTextBlock.Text += sportRadioButton.Content; }
+            if (musicRadioButton.IsChecked == true) { resultTextBlock.Text += musicRadioButton.Content; }
+            if (foodRadioButton.IsChecked == true) { resultTextBlock.Text += foodRadioButton.Content; }
+        }
+
+        private void ClearSelectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            sportRadioButton.IsChecked = false;
+            musicRadioButton.IsChecked = false;
+            foodRadioButton.IsChecked = false;
+
+            resultTextBlock.Text = "Selected preferences: ";
+        }
+    }
+}
